dbd9ec994b073b5c23472f31f0865ceabed8786d
bashikochan <bashikojob325@gmail.com>
2022-11-21 18:09:30 +0900

    ActiveStorageの導入

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,9 @@
 class Collection < ApplicationRecord
+  belongs_to :user
   belongs_to :album_id
   belongs_to :version_id
+  has_one_attached :image
+
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   belongs_to :favemember
+  has_many :collections
 
   validates :nickname, presence: true
   validates :profile, presence: true
