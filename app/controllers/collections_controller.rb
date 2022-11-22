da5a5053d5aace05f47053f80c9fd293b141d01d
bashikochan <bashikojob325@gmail.com>
2022-11-22 14:47:42 +0900

    投稿機能の実装

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,25 @@
 class CollectionsController < ApplicationController
+
   def index
+    @collections = Collection.all
+  end
+
+  def new
+    @collection = Collection.new
+  end
+
+  def create
+    @collection = Collection.new(collection_params)
+    if @collection.save
+      redirect_to root_path    
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:image, :album_id, :version_id, :others).merge(user_id: current_user.id)
   end
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,6 @@
 class Collection < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :album_id
   belongs_to :version_id
 end
