2837b2d0d8744aedaca7cfcbc24a66a931c80f60
bashikochan <bashikojob325@gmail.com>
2022-11-22 22:50:30 +0900

    ユーザー機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@ class User < ApplicationRecord
   belongs_to :favemember
 
   validates :nickname, presence: true
+  validates :chemistry, presence: true
   validates :profile, presence: true
   # ジャンル選択が「ーーー」の時は保存できないようにする
   validates :favemember_id, numericality: { other_than: 1, message: "can't be blank" }
-
+  
 end
